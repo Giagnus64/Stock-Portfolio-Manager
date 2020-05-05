d7809eb5d67fffaa3d73227fc63ea3b6b2f10042
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-05-05 16:05:59 -0400

    tried to fix gitignore

--- Stock-Portfolio-Backend/app/models/stock.rb
+++ Stock-Portfolio-Backend/app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
-    has_many :transactions
+    has_many :transactions, dependent: :destroy
     has_many :users, through: :transactions
+
+    validates :company_name, presence:true, uniqueness:true
+    validates :symbol, presence:true, uniqueness:true
+    
 end
--- Stock-Portfolio-Backend/app/models/user.rb
+++ Stock-Portfolio-Backend/app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-    has_many :transactions
+    has_many :transactions, dependent: :destroy
     has_many :stocks, through: :transactions
     has_secure_password
+    
+    validates :name, presence:true
+    validates :email, uniqueness:true, presence:true
+    validates :account_balance, presence:true
 end
