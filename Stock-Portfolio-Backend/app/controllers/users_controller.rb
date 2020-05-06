4f65d348108630c468e4187eac8c683d496c7d80
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-05-06 00:08:31 -0400

    Added login-Controller, edited app controller and user controller

--- Stock-Portfolio-Backend/app/controllers/users_controller.rb
+++ Stock-Portfolio-Backend/app/controllers/users_controller.rb
@@ -1,2 +1,51 @@
 class UsersController < ApplicationController
+
+    #skip_before_action :authorized, only:[:create]
+
+    #create new user
+    def create
+        #get new params
+        new_user_params = user_params
+        #create new user
+        user = User.create(new_user_params)
+        if(user.valid?)
+            #generate auth token
+            token = encode_token({user_id: user.id})
+            #generate user hash
+            userHash = user.as_json(only: [:id, :email])
+            #add token to hash
+            userHash[:token] = token
+            #respond with user's info as json
+            render json: userHash, status: :created
+        else
+            #if an error occurs, print errors
+            render json: {messages: user.errors.full_messages}
+        end
+    end
+
+    #show user portfolio
+    def show
+        user
+    end
+
+    def update
+    #update user money when a transaction is made
+    end
+
+#    #Extra methods if time permits
+#     def edit
+    
+#     end
+
+#     def destroy 
+    
+#     end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :name)
+    end
+    
+
 end
--- Stock-Portfolio-Backend/config/routes.rb
+++ Stock-Portfolio-Backend/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :stocks
+  #resources :stocks, only:[:index, :create]
   resources :transactions
   resources :users
+  #get "user_recipes/:id", to: 'user_recipes#index', as: "saved_recipes"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
